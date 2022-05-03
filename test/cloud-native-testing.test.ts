eef28ba3e77c471f7344b6aa5de98a0cc56866a7
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2022-05-03 22:58:33 +0200

    Initial commit

---
+++ test/cloud-native-testing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudNativeTesting from '../lib/cloud-native-testing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-native-testing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudNativeTesting.CloudNativeTestingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
